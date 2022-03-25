9fb5704d1fe45df467c9d6d5aa03e2bb25f7c059
Nghia Dang <vannghiadang79@gmail.com>
2022-03-25 21:04:41 +0700

    part 1

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/screens/add_post_screen.dart';
+
+const webScreenSize = 600;
+const homeScreenItems = [
+  Text('news feed'),
+  Text('search'),
+  AddPostScreen(),
+  Text('notify'),
+  Text('profile'),
+];
